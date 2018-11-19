a4379db5ef6e8a89c8cc5c360f9bd81ef33a9e48
Kevin Olson <kolson@Administrators-MacBook-Pro-2.local>
2018-11-19 15:46:19 -0500

    Added Components Directory and Synths JSON file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import RandomMusic from "./components/RandomMusic";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Create Random Music</h1>
+        <h2>About</h2>
+        <p>
+          I want to explore the various sounds that Logic Pro has to offer.
+          Decided to create this random sounds app that will choose a variey of
+          midi sounds to experiment with
+        </p>
+        <h2>Rules</h2>
+        <p>
+          Must use all of options that are given in some way to create a piece
+          of music
+        </p>
+        <RandomMusic />
       </div>
     );
   }
---
+++ src/components/RandomMusic.js
@@ -0,0 +1,118 @@
+import React from "react";
+import * as data from "../synths.json";
+
+class RandomMusic extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getSynth2 = this.getSynth2.bind(this);
+
+    this.yesNo = this.yesNo.bind(this);
+    this.alteredYes = this.alteredYes.bind(this);
+    this.getDrums = this.getDrums.bind(this);
+    this.getSynth = this.getSynth.bind(this);
+
+    this.onClick = this.onClick.bind(this);
+    this.getAcousticGuitar = this.getAcousticGuitar.bind(this);
+    this.getBass = this.getBass.bind(this);
+    this.state = {
+      number: null,
+      sythList: [],
+      numSynths: null,
+      acousticGuitar: null,
+      bass: null,
+      drums: null
+    };
+  }
+
+  getSynth2() {
+    var s = [];
+    for (var i = 0; i < data.synths.length; i++) {
+      s.push(data.synths[i]);
+      console.log(s[i]);
+    }
+  }
+
+  getSynth() {
+    var allSynths = [];
+    for (var j = 0; j < data.synths.length; j++) {
+      allSynths.push(data.synths[j]);
+    }
+    var numSynths = Math.floor(Math.random() * 4);
+    var finalSynths = [];
+    console.log("Number of Synths " + numSynths);
+    for (var i = 0; i < numSynths; i++) {
+      finalSynths[i] =
+        allSynths[Math.floor(Math.random() * allSynths.length)] + " ";
+      console.log("synth " + finalSynths[i]);
+    }
+    this.setState({
+      numSynths,
+      synthList: finalSynths
+    });
+  }
+
+  yesNo() {
+    var answer = "";
+    const num = 1 + Math.floor(Math.random() * 2);
+    if (num === 1) {
+      answer = "yes";
+    } else {
+      answer = "no";
+    }
+    return answer;
+  }
+
+  alteredYes() {
+    var answer = "";
+    const num = 1 + Math.floor(Math.random() * 10);
+    if (num >= 3) {
+      answer = "yes";
+    } else {
+      answer = "no";
+    }
+
+    return answer;
+  }
+
+  getDrums() {
+    var answer = this.yesNo();
+    this.setState({
+      drums: answer
+    });
+  }
+
+  getAcousticGuitar() {
+    var answer = this.yesNo();
+    this.setState({
+      acousticGuitar: answer
+    });
+  }
+
+  getBass() {
+    var answer = this.yesNo();
+    this.setState({
+      bass: answer
+    });
+  }
+
+  onClick() {
+    this.getSynth();
+    this.getAcousticGuitar();
+    this.getDrums();
+    this.getBass();
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClick}>Get Your Random Music!</button>
+        <h3> Drums : {this.state.drums}</h3>
+        <h3>Number of Synths: {this.state.numSynths}</h3>
+        <p>{this.state.synthList}</p>
+        <h3>Acoustic Guitar: {this.state.acousticGuitar}</h3>
+        <h3>Bass Guitar: {this.state.bass}</h3>
+      </div>
+    );
+  }
+}
+export default RandomMusic;
